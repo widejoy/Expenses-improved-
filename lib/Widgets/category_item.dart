37926780155cdf3f53019b16f8a7a3b3d6caec73
widejoy <rogerantony40@ug.cusat.ac.in>
2023-06-03 07:40:38 +0530

    added message if list is empty

--- lib/Widgets/category_item.dart
+++ lib/Widgets/category_item.dart
@@ -13,44 +13,66 @@ class CategoryItem extends StatefulWidget {
 
 class _CategoryItemState extends State<CategoryItem> {
   final List<GroceryItem> _groceryitems = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Groceries'),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              final newitem = await Navigator.of(context).push<GroceryItem>(
-                MaterialPageRoute(
-                  builder: (context) => const NewItem(),
-                ),
-              );
-              if (newitem == null) {
-                return;
-              } else {
-                setState(() {
-                  _groceryitems.add(newitem);
-                });
-              }
-            },
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: _groceryitems.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_groceryitems[index].name),
-          leading: Container(
-              width: 24,
-              height: 24,
-              color: _groceryitems[index].category.itemcolor),
-          trailing: Text(
-            _groceryitems[index].quantity.toString(),
-          ),
+    Widget elements = ListView.builder(
+      itemCount: _groceryitems.length,
+      itemBuilder: (context, index) => ListTile(
+        title: Text(_groceryitems[index].name),
+        leading: Container(
+            width: 24,
+            height: 24,
+            color: _groceryitems[index].category.itemcolor),
+        trailing: Text(
+          _groceryitems[index].quantity.toString(),
         ),
       ),
     );
+    if (_groceryitems.isEmpty) {
+      setState(
+        () {
+          elements = const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'uhuh looks like ur list is empty',
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Text('try adding some')
+              ],
+            ),
+          );
+        },
+      );
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Groceries'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                final newitem = await Navigator.of(context).push<GroceryItem>(
+                  MaterialPageRoute(
+                    builder: (context) => const NewItem(),
+                  ),
+                );
+                if (newitem == null) {
+                  return;
+                } else {
+                  setState(() {
+                    _groceryitems.add(newitem);
+                  });
+                }
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: elements);
   }
 }
